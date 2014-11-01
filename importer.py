f7491b5fddf10fb79726baa0cca1e9775afdf3ae
Byron Williams <byron@cloudfindhq.com>
2014-11-01 13:55:10 +0000

    Add importer

---
+++ importer.py
@@ -0,0 +1,62 @@
+import json
+import csv
+import os
+import datetime
+import re
+import bson
+
+
+DATEFMT = "%Y-%m-%dT00:00:00+00:00"
+
+def run_import():
+    files = os.listdir("data")
+
+    docs = []
+
+    for f in files:
+        with open("data/" + f, "rb") as csvfile:
+            reader = csv.reader(csvfile)
+
+            fields = []
+            print(f)
+
+            for idx, item in enumerate(reader):
+                if idx == 0:
+                    fields = [x.replace(" ", "") for x in item]
+                    print(fields)
+                else:
+                    doc = dict(zip(fields, item))
+
+                    if len(doc["Amount"]):
+                        doc["Amount"] = float(re.sub("[^0-9]", "", doc["Amount"]))
+
+                    if "Date" in doc.keys():
+                        date_parts = doc["Date"].split("/")
+                        date_parts.reverse()
+
+                        if len(date_parts[0]) == 2:
+                            date_parts[0] = "20" + date_parts[0]
+
+                        if len(date_parts) == 3:
+                            doc["Date"] = datetime.date(*[int(x) for x in date_parts]).strftime(DATEFMT)
+
+                    elif "Payment Date" in doc.keys():
+                        date_parts = doc["Payment Date"].split("/")
+                        date_parts.reverse()
+
+                        if len(date_parts[0]) == 2:
+                            date_parts[0] = "20" + date_parts[0]
+
+                        if len(date_parts) == 3:
+                            doc["Date"] = datetime.date(*[int(x) for x in date_parts]).strftime(DATEFMT)
+
+                    doc["Votes"] = 0
+                    docs.append(doc)
+
+    with open("out.json", "w") as outf:
+        json.dump(docs, outf)
+
+
+
+if __name__ == "__main__":
+    run_import()
